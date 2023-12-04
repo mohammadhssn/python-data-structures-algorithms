f75932088e60d25af416f1ae0052894ca65fe3d0
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2023-12-04 20:41:58 +0330

    feat(Big O): Add O(1)

--- 01-Big O/Drop-Non-Dominants.py
+++ 01-Big O/Drop-Non-Dominants.py
@@ -1,4 +1,4 @@
-def print_items(n):
+def print_items(n: int) -> None:
     for i in range(n):
         for j in range(n):
             print(i, j)
---
+++ 01-Big O/O(1).py
@@ -0,0 +1,5 @@
+def add_items(n: int) -> int:
+    return n + n + n
+
+
+print(add_items(10))
