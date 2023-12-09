e2d5e2a98fa2cbd42770c72f0fabe1ef56328b7d
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2023-12-09 21:14:40 +0330

    feat(012-LL-Find-Middle-Node): Added finde middle method

---
+++ 02-Linked Lists/012-LL-Find-Middle-Node.py
@@ -0,0 +1,152 @@
+from typing import Any
+
+
+class Node:
+    def __init__(self, value: Any) -> None:
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value: Any) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self) -> None:
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value: Any) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self) -> Node | None:
+        if self.length == 0:
+            return None
+
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def prepend(self, value: Any) -> bool:
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self) -> Node | None:
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index: int) -> Node | None:
+        if index < 0 or index > self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    
+    def set_value(self, index: int, value: Any) -> bool:
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index: int, value: Any) -> bool:
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        else:
+            new_node = Node(value)
+            temp = self.get(index - 1)
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1
+            return True 
+
+    def remove(self, index: int) -> Node | None:
+        if index < 0 or index > self.length:
+            return None
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop()
+        else:
+            prev = self.get(index - 1)
+            temp = prev.next
+            prev.next = temp.next
+            temp.next = None
+            self.length -= 1
+            return temp
+
+    def reverse(self) -> None:
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+    
+    def find_middle(self) -> Node:
+        # Initialize two pointers to the head of the list
+        slow = self.head
+        fast = self.head
+        
+        # Traverse the list with the fast pointer moving twice
+        # as fast as the slow pointer
+        while fast is not None and fast.next is not None:
+            slow = slow.next  # Move slow pointer one step
+            fast = fast.next.next  # Move fast pointer two steps
+            
+        # When the fast pointer reaches the end, the slow
+        # pointer will be at the middle node
+        return slow
+    
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+print(my_linked_list.find_middle().value)  # 3
