782a065bd1246056bd2a2cb695fc5aa38951100e
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2023-12-10 20:52:00 +0330

    feat(007-DLL-Set): Added DDL Set Value method

--- 03-Doubly Linked Lists/007-DLL-Set.py
+++ 03-Doubly Linked Lists/007-DLL-Set.py
@@ -85,27 +85,43 @@ class DoublyLinkedList:
             for _ in range(self.length - 1, index, -1):
                 temp = temp.prev
         return temp
-
-
-my_doubly_linked_list = DoublyLinkedList(0)
-my_doubly_linked_list.append(1)
-my_doubly_linked_list.append(2)
+    
+    def set_value(self, index: int, value: Any) -> bool:
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+  
+my_doubly_linked_list = DoublyLinkedList(11)
 my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(23)
+my_doubly_linked_list.append(7)
 
-print('Get node from first half of DLL:')
-print(my_doubly_linked_list.get(1).value)
+print('DLL before set_value():')
+my_doubly_linked_list.print_list()
 
-print('\nGet node from second half of DLL:')
-print(my_doubly_linked_list.get(2).value)
+my_doubly_linked_list.set_value(1,4)
+
+print('\nDLL after set_value():')
+my_doubly_linked_list.print_list()
+
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    Get node from first half of DLL:
-    1
+    DLL before set_value():
+    11
+    3
+    23
+    7
 
-    Get node from second half of DLL:
-    2
+    DLL after set_value():
+    11
+    4
+    23
+    7
 
 """
