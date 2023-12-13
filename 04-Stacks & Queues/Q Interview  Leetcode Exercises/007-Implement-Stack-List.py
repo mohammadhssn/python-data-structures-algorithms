353ba7d179071803b8df6345f2298e72ea5b858d
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2023-12-13 18:23:42 +0330

    feat(007-Implement-Stack-List): Implement Stack Using a List

--- 04-Stacks & Queues/Q Interview  Leetcode Exercises/007-Implement-Stack-List.py
+++ 04-Stacks & Queues/Q Interview  Leetcode Exercises/007-Implement-Stack-List.py
@@ -1,3 +1,12 @@
+"""
+Instructions:
+Stack: Implement Stack Using a List (⚡Interview Question)
+In the Stack: Intro video we discussed how stacks are commonly implemented using a list instead of a linked list.
+
+Create a constructor for Class Stack that implements a new stack with an empty list.
+"""
+
+
 class Stack:
     def __init__(self) -> None:
         self.stack_list = []
