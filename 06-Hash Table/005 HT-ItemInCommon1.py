be530def77f1a23a6bde781a9d504715d3de087a
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2023-12-16 20:49:13 +0330

    feat(): Added HT Item in Common Method

---
+++ 06-Hash Table/005 HT-ItemInCommon1.py
@@ -0,0 +1,11 @@
+def item_in_common(list1: list, list2: list) -> bool:
+    for i in list1:
+        for j in list2:
+            if i == j:
+                return True
+    return False
+
+list1 = [1, 3, 5]
+list2 = [2, 4, 6]
+
+print(item_in_common(list1, list2))
