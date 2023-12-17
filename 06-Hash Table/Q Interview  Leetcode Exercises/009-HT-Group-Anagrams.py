1e70d71b3d164e958aa308de35aa4476f64c1463
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2023-12-17 20:55:04 +0330

    feat(009-HT-Group-Anagrams): Added HT Group-Anagrams Method

---
+++ 06-Hash Table/Q Interview  Leetcode Exercises/009-HT-Group-Anagrams.py
@@ -0,0 +1,57 @@
+"""
+Instructions: 
+HT: Group Anagrams (⚡Interview Question)
+You have been given an array of strings, where each string may contain only lowercase English letters. You need to write a function group_anagrams(strings) that groups the anagrams in the array together using a hash table (dictionary). The function should return a list of lists, where each inner list contains a group of anagrams.
+
+For example, if the input array is ["eat", "tea", "tan", "ate", "nat", "bat"], the function should return [["eat","tea","ate"],["tan","nat"],["bat"]] because the first three strings are anagrams of each other, the next two strings are anagrams of each other, and the last string has no anagrams in the input array.
+
+You need to implement the group_anagrams(strings) function and return a list of lists, where each inner list contains a group of anagrams according to the above requirements.
+"""
+
+def group_anagrams(strings: list[str]) -> list[list[str]]:
+    # create an empty hash table
+    anagram_groups = {}
+ 
+    # iterate through each string in the array
+    for string in strings:
+        # sort each string to get its canonical form
+        # sorted('eat') returns ['a', 'e', 't']
+        # ''.join(['a', 'e', 't']) coverts the array of chars to 'aet' string
+        canonical = ''.join(sorted(string))
+ 
+        # check to see if the canonical form of the string exists in the hash table
+        if canonical in anagram_groups:
+            # if it does then add the string there
+            anagram_groups[canonical].append(string)
+        else:
+            # otherwise create new canonical form and add the string there
+            anagram_groups[canonical] = [string]
+ 
+    # convert the hash table to a list of lists
+    return list(anagram_groups.values())
+
+
+print("1st set:")
+print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+print("\n2nd set:")
+print(group_anagrams(["abc", "cba", "bac", "foo", "bar"]))
+
+print("\n3rd set:")
+print(group_anagrams(["listen", "silent", "triangle", "integral", "garden", "ranged"]))
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    1st set:
+    [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+
+    2nd set:
+    [['abc', 'cba', 'bac'], ['foo'], ['bar']]
+
+    3rd set:
+    [['listen', 'silent'], ['triangle', 'integral'], ['garden', 'ranged']]
+
+"""
