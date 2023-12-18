090e3ece2f405fffbdec60182acfd787077987c1
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2023-12-18 18:56:40 +0330

    feat(001-GR-Remove-Edge): Added Remove Edge Method

---
+++ 07-Graphs/003-GR-Remove-Edge.py
@@ -0,0 +1,63 @@
+class Graph:
+    def __init__(self) -> None:
+        self.adj_list = {}
+
+    def print_graph(self) -> None:
+        for vertex in self.adj_list:
+            print(vertex, ':', self.adj_list[vertex])
+
+    def add_vertex(self, vertex: str) -> bool:
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    def add_edge(self, v1: str, v2: str) -> bool:
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+    def remove_edge(self, v1: str, v2: str) -> bool:
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].remove(v2)
+            self.adj_list[v2].remove(v1)
+            return True
+        return False
+
+
+my_graph = Graph()
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+
+my_graph.add_edge('A','B')
+my_graph.add_edge('B','C')
+my_graph.add_edge('C','A')
+
+print('Graph before remove_edge():')
+my_graph.print_graph()
+
+
+my_graph.remove_edge('A','C')
+
+
+print('\nGraph after remove_edge():')
+my_graph.print_graph()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Graph before remove_edge():
+    A : ['B', 'C']
+    B : ['A', 'C']
+    C : ['B', 'A']
+
+    Graph after remove_edge():
+    A : ['B']
+    B : ['A', 'C']
+    C : ['B']
+    
+"""
